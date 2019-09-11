f3a5fd19d971d1c7c17a873c466213aead56439e
DenFedotov <fdv984@gmail.com>
2019-09-11 14:48:46 +0300

    Простые числа.[#177885]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -3,14 +3,9 @@ package ru.job4j.loop;
 public class Factorial {
     public int calc(int n) {
         int result = 1;
-        if(n==0 || n==1){
-            result =1;
-        } else {
-            for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
                 result*=i;
-            }
         }
-
         return result;
     }
 }
