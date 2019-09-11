98dc1820c9a29543fb8484e76e90ca7c9a049cb7
DenFedotov <fdv984@gmail.com>
2019-09-11 15:40:11 +0300

    Ипотека.[#177886]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount = (int) (amount * percent / 100 + amount - salary);
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        Mortgage m = new Mortgage();
+        System.out.println(m.year(100, 70, 50));
+    }
+}
