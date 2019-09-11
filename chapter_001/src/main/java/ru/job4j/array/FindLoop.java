de2703a3e0c99a6e2d86186d72c84b37622509dd
DenFedotov <fdv984@gmail.com>
2019-09-11 21:01:29 +0300

    Сортировка выборкой.[#177903]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -22,4 +22,17 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = i;
+            for (int j = i+1; j < data.length; j++) {
+                if(data[min]>data[j]) min = j;
+            }
+            int b = data[i];
+            data[i] = data[min];
+            data[min] = b;
+        }
+        return data;
+    }
 }
