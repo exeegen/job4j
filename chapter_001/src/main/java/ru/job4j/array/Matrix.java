8225700781c197336a1c0767dfd6188664467c26
DenFedotov <fdv984@gmail.com>
2019-09-11 21:45:43 +0300

    Выигрышные комбинации в сокобан[#177895]

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class Matrix {
     public int[][] multiple(int size) {
         int[][] table = new int[size][size];
@@ -7,8 +9,18 @@ public class Matrix {
             for (int j = 0; j < size; j++) {
                 table[i][j] = (i+1)*(j+1);
             }
-
         }
         return table;
+    }
+
+    public static void main(String[] args) {
+        Matrix m = new Matrix();
+        int[][] arr = m.multiple(9);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println("");
+        }
     }
 }
