96aa18755bbcd502079802a146808986a28607ae
DenFedotov <fdv984@gmail.com>
2019-09-11 16:43:56 +0300

    JavaDoc. Code convention[#177882]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate
+ *
+ * @author exeegen (noname@yandex.ru)
+ */
+public class Calculate {
+
+    /**
+     * Main
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+
+    }
+}
