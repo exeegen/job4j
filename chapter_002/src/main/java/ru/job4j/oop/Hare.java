a078ff9e75786aa2cb48043287a70c4b75d90ec1
DenFedotov <fdv984@gmail.com>
2019-09-14 19:32:59 +0300

    Петя и красная шапочка.[#178012]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Hare.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Hare {
+    Ball eat (Ball ball){
+        System.out.println(ball.go() + "Hare");
+        return ball;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Wolf {
+    Ball eat (Ball ball){
+        System.out.println(ball.go() + "Wolf");
+        return ball;
+    }
+}
