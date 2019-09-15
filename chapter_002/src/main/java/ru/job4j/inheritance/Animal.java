fa9fb52c6fb3e3d40b10dbe13e7433bc8fef0af6
DenFedotov <fdv984@gmail.com>
2019-09-15 08:48:44 +0300

    Перегрузка конструктора.[#178019]

--- chapter_002/src/main/java/ru/job4j/inheritance/Animal.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Animal.java
@@ -1,8 +1,15 @@
 package ru.job4j.inheritance;
 
 public class Animal {
+
+    String name;
+
     public Animal(){
         // вызов конструктора Object автоматически
         System.out.println(Animal.class.getSimpleName());
+    }
+
+    public Animal(String name){
+        this.name = name;
     }
 }
--- chapter_002/src/main/java/ru/job4j/inheritance/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Tiger.java
@@ -1,12 +1,12 @@
 package ru.job4j.inheritance;
 
 public class Tiger extends Predator{
-    public Tiger(){
-        super();
+    public Tiger(String name){
+        super(name);
         System.out.println(Tiger.class.getSimpleName());
     }
 
     public static void main(String[] args) {
-        new Tiger();
+        new Tiger("Tigra");
     }
 }
