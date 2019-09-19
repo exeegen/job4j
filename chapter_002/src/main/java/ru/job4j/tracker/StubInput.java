65b4044c541144d6296a18cd6a0ed8074e53d511
DenFedotov <fdv984@gmail.com>
2019-09-19 21:32:23 +0300

    Обеспечить бесперебойную работу приложения Tracker.[#177996]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -16,6 +16,6 @@ public class StubInput implements Input {
 
     @Override
     public int ask(String question, List<Integer> range) {
-        throw new UnsupportedOperationException();
+        return Integer.valueOf(ask(question));
     }
 }
